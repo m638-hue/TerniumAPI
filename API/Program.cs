4e687b7b2708332a6c936ac93cf9a9a96d8ff06e
mmauro2152 <A01282152@itesm.mx>
2023-05-26 14:32:27 -0600

    Prueba Azure

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,8 @@
 using Microsoft.Data.SqlClient;
 
-using (SqlConnection connection = new SqlConnection("Server=tcp:ternium-db.database.windows.net,1433;Initial Catalog=terniumDB;Persist Security Info=False;User ID=terniumAdmin;Password=Ternium44672?4_2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
-{
-    connection.Open();
+//using (SqlConnection connection = new SqlConnection("Server=tcp:ternium-db.database.windows.net,1433;Initial Catalog=terniumDB;Persist Security Info=False;User ID=terniumAdmin;Password=Ternium44672?4_2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+//{
+//    connection.Open();
     // Do work here.  
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.Build();
@@ -11,4 +11,4 @@ using (SqlConnection connection = new SqlConnection("Server=tcp:ternium-db.datab
 
 
     app.Run();
-}
+//}
