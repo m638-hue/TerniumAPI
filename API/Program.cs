64e1cf24eb3af287e31c9816a7f55053aeab1cd7
mmauro2152 <A01282152@itesm.mx>
2023-05-30 16:48:26 -0600

    T

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,14 @@
 using Microsoft.Data.SqlClient;
 
-//using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("SQLConnectionString")))
-//{
-    //connection.Open();
+using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("SQLAZURECONNSTR_TERNIUMDB")))
+{
+    connection.Open();
     // Do work here.  
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.Build();
 
-    app.MapGet("/", () => Environment.GetEnvironmentVariable("SQLAZURECONNSTR_SQLConnectionString"));
+    app.MapGet("/", () => Environment.GetEnvironmentVariable("SQLAZURECONNSTR_TERNIUMDB"));
 
 
     app.Run();
-//}
+}
