be21c17a96cd779b9f39d14f676ec8e4c780d938
mmauro2152 <A01282152@itesm.mx>
2023-05-30 15:53:11 -0600

    t

--- API/Program.cs
+++ API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Data.SqlClient;
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.Build();
 
-    app.MapGet("/", () => Environment.GetEnvironmentVariable("SQLConnectionString"));
+    app.MapGet("/", () => Environment.GetEnvironmentVariable("SQLAZURECONNSTR_SQLConnectionString"));
 
 
     app.Run();
