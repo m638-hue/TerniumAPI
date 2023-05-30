aeccc9038b998100badf962531178a11ff0bb37e
mmauro2152 <A01282152@itesm.mx>
2023-05-30 15:40:51 -0600

    Testing env variable

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,14 @@
 using Microsoft.Data.SqlClient;
 
-using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("SQLConnectionString")))
-{
-    connection.Open();
+//using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("SQLConnectionString")))
+//{
+    //connection.Open();
     // Do work here.  
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.Build();
 
-    app.MapGet("/hello", () => "Hello World!");
+    app.MapGet("/hello", () => Environment.GetEnvironmentVariable("SQLConnectionString"));
 
 
     app.Run();
-}
+//}
