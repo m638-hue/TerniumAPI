50a85d3c0c18b7d8d79f3e01aea8ab73ca5cce93
mmauro2152 <A01282152@itesm.mx>
2023-05-30 18:05:23 -0600

    query test

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,31 @@
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+
+ 
 
 using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("SQLAZURECONNSTR_TERNIUMDB")))
 {
     connection.Open();
-    // Do work here.  
+    SQLReader sQLReader = new SQLReader(connection);
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.Build();
 
-    app.MapGet("/", () => Environment.GetEnvironmentVariable("SQLAZURECONNSTR_TERNIUMDB"));
+    app.MapGet("/", () => sQLReader.readQueryResult("select * from users");
 
 
     app.Run();
+}
+
+class SQLReader
+{
+    private SqlConnection connection;
+    public SQLReader(SqlConnection connection) => this.connection = connection;
+
+    public string readQueryResult(string query)
+    {
+        using (SqlDataReader reader = new SqlCommand(query, connection).ExecuteReader())
+        {
+            return reader.Read().ToString();
+        }
+    }
 }
