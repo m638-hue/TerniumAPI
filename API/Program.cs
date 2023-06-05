dec9ea2941ecd553e610a29a0cc76548a5c15adc
mmauro2152 <A01282152@itesm.mx>
2023-06-05 15:05:16 -0600

    test

--- API/Program.cs
+++ API/Program.cs
@@ -1,29 +1,36 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Tokens;
 
- 
-
-using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("SQLAZURECONNSTR_TERNIUMDB")))
+using (SqlConnection connection = new SqlConnection("Server=tcp:ternium-db.database.windows.net,1433;Initial Catalog=terniumDB;Persist Security Info=False;User ID=terniumAdmin;Password=Ternium44672?4_2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
 {
+    SQLReader sQLReader = new SQLReader();
     connection.Open();
-    SQLReader sQLReader = new SQLReader(connection);
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.Build();
 
-    app.MapGet("/", () => sQLReader.readQueryResult("select * from users"));
+    try
+    {
+        app.MapGet("/", () => sQLReader.readQueryResult(new SqlCommand("SELECT * FROM users;", connection)));
+    }
+    catch (Exception ex) { app.MapGet("/", () => ex.Message); }
+
     app.Run();
 }
 
 class SQLReader
 {
-    private SqlConnection connection;
-    public SQLReader(SqlConnection connection) => this.connection = connection;
-
-    public string readQueryResult(string query)
+    public string readQueryResult(SqlCommand query)
     {
-        using (SqlDataReader reader = new SqlCommand(query, connection).ExecuteReader()) 
+        try
         {
-            return reader.Read().ToString();
+            using (SqlDataReader reader = query.ExecuteReader())
+            {
+                string s = "";
+                while (reader.Read()) { s += reader.GetString(reader.GetOrdinal("nombres")); }
+
+                return s;
+            }
         }
+        catch (Exception ex) { return ex.Message + "\n" + ex.StackTrace; }
     }
 }
